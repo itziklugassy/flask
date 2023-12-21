46b03e7085dd92f83d0f3f09daf876067bfe37ea
Itzik Lugassy <itziklugassy@h-MBP-sl-Itzik.lan>
2023-12-21 09:30:48 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contactt")
+def contactt():
+    return render_template("contactt.html")
+
+
+if __name__== "__main__":
+    app.run(debug=True)
